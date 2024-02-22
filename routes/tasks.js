3d62d4788c30f2323f0daa71217551da6badeaa1
Bryan Turrubiates <bryanturrubiates@Bryans-MacBook-Air.local>
2024-02-22 07:00:15 -0600

    feat: (Tasks) Added tasks GET

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
+import tasks from '../resources/tasks.json' with { type: 'json' }
 
 export const taskRouting = Router()
 
 
 taskRouting.get('/', (req, res) => {
-  res.send("OBTENIENDO LAS TAREAS")
+  res.json(tasks).status(200)
 })
-
 
 taskRouting.post('/', (req, res) => {
   res.send("AGREGANDO TAREA")

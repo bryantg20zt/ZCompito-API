5dd4eee08896896ff51ba96984dadde0e912c371
BryanTurrubiates <bryanturrubiates@outlook.com>
2024-05-22 08:49:53 -0500

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express'
+import express, {Router} from 'express'
 import { taskRouting } from './routes/tasks.js' 
 import { authRouting } from './routes/auth.js'
 
 export const app = express()
-
+const routerMain = Router()
 
 app.use(express.json());
+
 app.use('/api/auth', authRouting)
 app.use('/api/tasks', taskRouting)
 
